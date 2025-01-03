6468fc47d53f5b71c3f26157756540badfc39e2d
1F616EMO <root@1f616emo.xyz>
2025-01-03 17:18:20 +0800

    Do real caching...

--- init.lua
+++ init.lua
@@ -5,56 +5,87 @@
 
 local S = core.get_translator("babelfish_mail")
 
-local mail_cache = setmetatable({}, {
-    __mode = "k",
-})
+---@type { [table] : { [string] : string[] } }
+local mail_queue = {}
 
-local function translate_mail(mail_msg, target, callback)
-    local mail_cache_data = mail_cache[mail_msg]
-    if not mail_cache_data then
-        mail_cache_data = {}
-        mail_cache[mail_msg] = mail_cache_data
-    end
-
-    mail_cache_data.translations = mail_cache_data.translations or {}
-    if mail_cache_data.translations[target] and mail_cache_data.source then
-        return callback(mail_cache_data.translations[target], mail_cache_data.source)
-    end
-
-    local new_body_original =
-        "Subject: " .. mail_msg.subject .. "\n\n" ..
-        mail_msg.body
-
-    local specified_source = "auto"
-    for _, line in ipairs(string.split(mail_msg.body, "\r?\n", false, -1, true)) do
+local function detect_mail_language(body)
+    for _, line in ipairs(string.split(body, "\r?\n", false, -1, true)) do
         local _, _, detected = string.find(line, "^#Sourcelang: ([a-zA-Z-_]+)$")
         if detected then
             detected = babelfish.validate_language(detected)
             if detected then
-                specified_source = detected
-                break
+                return detected
             end
         end
     end
-    mail_cache_data.source = specified_source
+    return "auto"
+end
 
-    return babelfish.translate(specified_source, target, new_body_original, function(succeed, translated, source)
+-- Send without putting into the sender's box
+local function direct_send(target, m)
+    local msg = {
+		id = mail.new_uuid(),
+		from = m.from,
+		to = m.to,
+		cc = m.cc,
+		bcc = m.bcc,
+		subject = m.subject,
+		body = m.body,
+		time = os.time(),
+	}
+    if not core.player_exists(target) then return end
+
+    for _, on_player_receive in ipairs(mail.registered_on_player_receives) do
+        if on_player_receive(target, msg) then
+            break
+        end
+    end
+
+    for i=1, #mail.registered_on_receives do
+		if mail.registered_on_receives[i](m) then
+			break
+		end
+	end
+end
+
+local function do_translate(original_mail_msg, language, targets)
+    local new_body_original =
+        "Subject: " .. original_mail_msg.subject .. "\n\n" ..
+        original_mail_msg.body
+    local source_language = detect_mail_language(original_mail_msg.body)
+
+    return babelfish.translate(source_language, language, new_body_original, function(succeed, translated, source)
         if not succeed then
-            return core.log("warning", string.format("Translated mail from %s to %s failed: %s",
-                mail_cache_data.source or "auto", target, dump(mail_msg)))
+            return core.log("warning", string.format("Translated mail from %s (detected: %s) to %s failed: %s",
+                source_language, source, language, dump(original_mail_msg)))
         end
         translated =
             S("Language detected: @1", source) .. "\n" ..
             S("If this is wrong, write #Sourcelang: <language code> in your message.") .. "\n\n" ..
             translated
-        mail_cache_data.source = source
-        mail_cache_data.translations[target] = translated
-        return callback(translated, source)
+
+        local mail_msg = {
+            from = original_mail_msg.from,
+            to = original_mail_msg.to,
+            cc = original_mail_msg.cc,
+            bcc = original_mail_msg.bcc,
+            subject = S("Translated: @1", original_mail_msg.subject),
+            body = translated,
+            _skip_babelfish_mail = true,
+        }
+
+        core.log("action", string.format("Translated mail from %s (detected: %s) to %s for %s: %s",
+            source_language, source, language, table.concat(targets, ", "), dump(mail_msg)))
+
+        for _, target in ipairs(targets) do
+            direct_send(target, mail_msg)
+        end
     end)
 end
 
 mail.register_on_player_receive(function(name, mail_msg)
-    if string.find(mail_msg.subject, string.char(0x1b), 1, true)
+    if mail_msg._skip_babelfish_mail
+        or string.find(mail_msg.subject, string.char(0x1b), 1, true)
         or string.find(mail_msg.body, string.char(0x1b), 1, true) then
         return
     end
@@ -64,19 +95,22 @@ mail.register_on_player_receive(function(name, mail_msg)
     local target = babelfish.get_player_preferred_language(name)
     if not target then return end
 
-    return translate_mail(mail_msg, target, function(translated, source)
-        if not translated or target == source then return end
+    mail_queue[mail_msg] = mail_queue[mail_msg] or {}
+    mail_queue[mail_msg][target] = mail_queue[mail_msg][target] or {}
+    mail_queue[mail_msg][target][#mail_queue[mail_msg][target]+1] = name
+end)
 
-        local success, err = mail.send({
-            from = mail_msg.from,
-            to = name,
-            subject = S("Translated: @1", mail_msg.subject),
-            body = translated,
-        })
-        if not success then
-            return core.log("error", "Failed to send translated mail: %s", err)
+core.register_globalstep(function()
+    local mail_msg, targets_list = next(mail_queue)
+    if mail_msg then
+        local language, targets = next(targets_list)
+        if language then
+            do_translate(mail_msg, language, targets)
+            mail_queue[mail_msg][language] = nil
+        else
+            mail_queue[mail_msg] = nil
         end
-    end)
+    end
 end)
 
 local setting_index = 1
@@ -94,3 +128,4 @@ mail.settings.babelfish_mail_translate = { -- luacheck: ignore
     tooltip = S("Translate mails not in your preferred language.")
 }
 mail.selected_idxs.babelfish_mail_translate = {} -- luacheck: ignore
+
